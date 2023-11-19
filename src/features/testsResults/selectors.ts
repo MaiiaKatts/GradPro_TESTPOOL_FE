9ec8f7b987dea406338c4049b7a336db5a9c571f
Iren25 <irenfrank.de@gmail.com>
2023-11-20 00:42:49 +0100

    changed crlf

--- src/features/testsResults/selectors.ts
+++ src/features/testsResults/selectors.ts
@@ -6,4 +6,5 @@ export const selectTestsResults = (state: RootState): TestResult[] =>
 	state.testsResults.testsResults;
 /*export const selectLastSavedTestResult = (state: RootState): TestResult | null =>
 	state.testsResults.lastSavedResult;*/
-export const selectError = (state: RootState): string | undefined => state.testsResults.error;
+export const selectError = (state: RootState): string | undefined =>
+	state.testsResults.error;
